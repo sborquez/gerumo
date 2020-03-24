bbff4e50faf751813d1225ddcdb1b10c1e52b22c
Sebastián Bórquez <sebastian.borquez.g@gmail.com>
2020-03-24 19:58:45 -0300

    Update __version__.py

--- gerumo/__version__.py
+++ gerumo/__version__.py
@@ -6,9 +6,9 @@ Information about the current version of the py-package-template package.
 """
 
 __title__ = 'gerumo'
-__description__ = 'Gamma Ray Event Reconstruction Uncertain Model'
+__description__ = 'Gamma Ray Event Reconstruction with Uncertain Models'
 __version__ = '0.0.1'
-__author__ = 'Sebastián Bórquez'
+__author__ = 'Sebastián Bórquez and Patricio Campaña'
 __author_email__ = 'sebastian.borquez.g@gmail.com'
 __license__ = 'GNU GPLv3'
 __url__ = 'https://github.com/sborquez/gerumo'
