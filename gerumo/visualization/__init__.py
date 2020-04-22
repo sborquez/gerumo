d968aa34134975f265a0afcaad8c729f04d37a0d
Sebastian Borquez <sebastian.borquez.g@gmail.com>
2020-04-22 02:10:37 -0400

    add some dummy show_ functions

--- gerumo/visualization/__init__.py
+++ gerumo/visualization/__init__.py
@@ -1,1 +1,14 @@
+"""
+Visualization
+=============
+
+Generate plots and show information.
+
+`plot` funtions generate a figure. These can be complex figures or independent
+figure. `show` functions generate individual and simples plots in an axis. This
+type of function can be used by `plot` to generate more elaborated figures.
+
+"""
+
 from .metrics import plot_model_training_history
+from .data import show_input_sample, show_target_sample
---
+++ gerumo/visualization/data.py
@@ -0,0 +1,14 @@
+"""
+Data Visualizations
+======================
+
+Generate plot for display dataset, images and targets.
+
+Here you can find dataset exploration and samples visualizations.
+"""
+
+def show_input_sample(input_image_sample, input_image_mode, input_features_sample, make_simple=False, ax=None):
+  pass
+
+def show_target_sample(target_sample, targets, target_mode, target_domains, ax=None):
+  pass
