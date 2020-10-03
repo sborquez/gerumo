1facfecdd1c60e8d5bbf46ad2d4da0fdab20f66c
capaths <pcampaa@gmail.com>
2020-10-03 15:53:53 -0300

    update params grid

--- gerumo/baseline/energy.py
+++ gerumo/baseline/energy.py
@@ -42,7 +42,7 @@ class EnergyModel:
         if param_grid is None:
             param_grid = {
                 "n_estimators": np.linspace(300, 500, 5, dtype=int),
-                "min_samples_split": np.arange(2, 6, dtype=int)
+                "min_samples_split": [2, 6]
             }
 
         grouped = dataset[["type", "mc_energy"] + self._features].groupby("type")
