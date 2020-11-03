55c5d9b1d687704cfc1dced8daa1f8044e625668
Sebastian Borquez <sborquez@ui01.hpc.utfsm.cl>
2020-11-03 13:51:54 -0300

    add: cd assembler evaluations

--- train/hpc/cctval/evaluate_assembler.sh
+++ train/hpc/cctval/evaluate_assembler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -p gpum
-#SBATCH -J assembler_bmo
+#SBATCH -p gpuk
+#SBATCH -J assembler_eval 
 #SBATCH --mail-user=sebastian.borquez@sansano.usm.cl
 #SBATCH --mail-type=ALL
 #SBATCH -o logs/output_assemblers_%j.log
@@ -17,34 +17,93 @@ source activate /user/s/sborquez/envs/gerumo
 echo "Running evaluate_assembler.sh"
 echo ""
 cd /user/s/sborquez/gerumo/train
-#echo "Umonna Assemblers"
-#folder="/user/s/sborquez/gerumo/train/config/cctval/alt_az/baseline/umonna_assembler_evaluations"
-#python evaluate.py --assembler "$folder/umonna_assembler.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/umonna_assembler_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/umonna_assembler_lst_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/umonna_assembler_mst_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/umonna_assembler_sst_hillas.json" --all --samples --predictions --results
 
-echo "BMO Assembler"
-folder="/user/s/sborquez/gerumo/train/config/cctval/alt_az/baseline/bmo_assembler_evaluations"
-python evaluate.py --assembler "$folder/bmo_assembler.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/bmo_assembler_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/bmo_assembler_lst_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/bmo_assembler_mst_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/bmo_assembler_sst_hillas.json" --all --samples --predictions --results
+# run assembler evaluation
+function run_evaluation {
+    folder=$1
+    config_file=$2
+    echo "configuration: $config_file"
+    python evaluate.py --assembler "$folder/$config_file.json" --all --samples --predictions --results
+}
 
+# echo "ALT AZ"
+# echo "======="
+# echo "Umonna Assemblers"
+# folder="/user/s/sborquez/gerumo/train/config/cctval/alt_az/baseline/umonna_assembler_evaluations"
+# configurations=(
+#     "umonna_assembler"
+#     "umonna_assembler_hillas"
+#     "umonna_assembler_lst_hillas"
+#     "umonna_assembler_mst_hillas"
+#     "umonna_assembler_sst_hillas"
+# )
+# for config_file in "${configurations[@]}";do
+#     run_evaluation $folder $config_file
+# done
 
-#echo "MVE Assembler"
-#python evaluate.py --assembler ./config/cctval/alt_az/baseline/mve_assembler.json --all --samples --results 
+# echo "BMO Assembler"
+# folder="/user/s/sborquez/gerumo/train/config/cctval/alt_az/baseline/bmo_assembler_evaluations"
+# configurations=(
+#     "bmo_assembler"
+#     "bmo_assembler_hillas"
+#     "bmo_assembler_lst_hillas"
+#     "bmo_assembler_mst_hillas"
+#     "bmo_assembler_sst_hillas"
+# )
+# for config_file in "${configurations[@]}";do
+#     run_evaluation $folder $config_file
+# done
 
-#echo "CNN Deterministic Assembler"
-#folder="/user/s/sborquez/gerumo/train/config/cctval/alt_az/baseline/cd_assembler_evaluations"
-#python evaluate.py --assembler "$folder/cd_assembler.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/cd_assembler_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/cd_assembler_lst_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/cd_assembler_mst_hillas.json" --all --samples --predictions --results
-#python evaluate.py --assembler "$folder/cd_assembler_sst_hillas.json" --all --samples --predictions --results
+# echo "CNN Deterministic Assembler"
+# folder="/user/s/sborquez/gerumo/train/config/cctval/alt_az/baseline/cd_assembler_evaluations"
+# configurations=(
+#     "cd_assembler"
+#     "cd_assembler_hillas"
+#     "cd_assembler_lst_hillas"
+#     "cd_assembler_mst_hillas"
+#     "cd_assembler_sst_hillas"
+# )
+# for config_file in "${configurations[@]}";do
+#     run_evaluation $folder $config_file
+# done
 
+echo "Energy"
+echo "======"
+# echo "Umonna Assemblers"
+# folder="/user/s/sborquez/gerumo/train/config/cctval/energy/baseline/umonna_assembler_evaluations"
+# configurations=(
+#     "umonna_assembler"
+#     "umonna_assembler_hillas"
+#     "umonna_assembler_lst_hillas"
+#     "umonna_assembler_mst_hillas"
+#     "umonna_assembler_sst_hillas"
+# )
+# for config_file in "${configurations[@]}";do
+#     run_evaluation $folder $config_file
+# done
 
-#echo "Testing assemblers"
-#python evaluate.py --assembler ./config/cctval/alt_az/baseline/tester_assembler.json --samples --predictions --results
+# echo "BMO Assembler"
+# folder="/user/s/sborquez/gerumo/train/config/cctval/energy/baseline/bmo_assembler_evaluations"
+# configurations=(
+#     "bmo_assembler"
+#     "bmo_assembler_hillas"
+#     "bmo_assembler_lst_hillas"
+#     "bmo_assembler_mst_hillas"
+#     "bmo_assembler_sst_hillas"
+# )
+# for config_file in "${configurations[@]}";do
+#     run_evaluation $folder $config_file
+# done
+
+echo "CNN Deterministic Assembler"
+folder="/user/s/sborquez/gerumo/train/config/cctval/energy/baseline/cd_assembler_evaluations"
+configurations=(
+    #"cd_assembler"
+    "cd_assembler_hillas"
+    #"cd_assembler_lst_hillas"
+    "cd_assembler_mst_hillas"
+    "cd_assembler_sst_hillas"
+)
+for config_file in "${configurations[@]}";do
+    run_evaluation $folder $config_file
+done
